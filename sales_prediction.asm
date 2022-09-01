e16bdffeea194ecdd9ace7ee5897de310b45601a
Stephen Aranda <aranda.stephen88@gmail.com>
2022-09-01 08:20:02 -0500

    added descriptor comment and file

---
+++ sales_prediction.asm
@@ -0,0 +1,6 @@
+# Author      : Stephen Aranda
+# File        : sales_prediction.asm
+# Date        : 9/1/2022
+# Description : Simple sales prediction app originally written in C ++ -- translated
+#             : into MIPS.
+
